ccf0a19f48d6afe34bd10abbbd352b201a1fe99b
Nicolas S <schuldtnicolass@gmail.com>
2022-12-06 12:11:05 +0100

    base endpoint

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,9 @@
-export default async (req, res) => {
-  const body = JSON.parse(req.body);
-  console.log(body);
-  res.status(200).json({ status: "Ok" });
+// vercel serverless function test
+const mailgun = require("mailgun-js");
+const DOMAIN = process.env.MAILGUN_DOMAIN;
+const api_key = process.env.MAILGUN_API_KEY;
+const sendMail = async (req, res) => {
+  return res.end(`Hello!`);
 };
+
+export default sendMail;
